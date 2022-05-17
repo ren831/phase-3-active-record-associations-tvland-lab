5ed7c798943fe7fb8f5c58e7118f2a5686f53d70
ren831 <rene83120@gmail.com>
2022-05-17 13:59:58 -0700

    commit tree 18a300f2d613745f97b3012a361d43516eb5a505

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |c|
+      c.actor.full_name
+    end
+  end
 end
